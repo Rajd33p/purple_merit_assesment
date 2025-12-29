7bf223b47e9038e942f6f0571fe07ffcc897a05f
Rajd33p <Rajd33p@RAJD33P-LAPTOP>
2025-12-30 01:49:34 +0530

    Update .gitignore and add User model with migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+
+    role = models.CharField(
+        max_length=10,
+        choices=[("admin", "Admin"), ("user", "User")],
+        default="user"
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=[("active", "Active"), ("inactive", "Inactive")],
+        default="Inactive"
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    last_login = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.email
